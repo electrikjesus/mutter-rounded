55b51f96edc3a73892b3eca607f7db1be26197c3
Jon West <electrikjesus@gmail.com>
2022-03-05 16:44:24 -0500

    mr-updater updates
    Make it cleanuop old installs by default and timeout on asking. 
    Also use notify-send commands to let the user know what is going on.

--- ubuntu_21.10/mr-updater/usr/local/bin/trigger-update.sh
+++ ubuntu_21.10/mr-updater/usr/local/bin/trigger-update.sh
@@ -7,7 +7,10 @@ TOOLPATH=$HPATH/.cache/mutter-rounded/mutter-rounded/tool
 PACKAGEPATH=$HPATH/.cache/mutter-rounded/mutter-rounded/ubuntu_21.10
 PACKAGESCRIPT=$HPATH/.cache/mutter-rounded/mutter-rounded/ubuntu_21.10/package.sh
 
-CLEANUP="false"
+# Send notice that update has started. 
+notify-send 'mr-updater' 'mutter-rounded update has started. We will let you know when it is complete.' --icon=dialog-information
+
+CLEANUP="true"
 if [ -f $TOOLPATH ]; then
 	CLEANUP="true"
 fi
@@ -15,10 +18,10 @@ fi
 echo "Mutter has been updated and has triggered the mutter-rounded updater script to update" 
 echo "you might experience color issues and no blue or rounded corner effects if this is not done."
 if [ -f $TOOLPATH ]; then
-	read -p "Do you want to update mutter-rounded (y/n)?" choice
+	read -t 10 -p "Do you want to update mutter-rounded (y/n)?" choice
 	case "$choice" in 
 	  y|Y ) echo "yes" && CLEANUP="true";;
-	  n|N ) echo "no" && echo "OK. You're the boss";;
+	  n|N ) echo "no" && CLEANUP="false" && echo "OK. You're the boss";;
 	  * ) echo "invalid";;
 	esac
 	
@@ -46,6 +49,12 @@ sudo dpkg -i $PACKAGEPATH/*.deb
 cd $HPATH
 
 echo "All set. Thanks for installing."
+
+# Send notice that update has finished. 
+notify-send 'mr-updater' 'mutter-rounded has been updated. Please restart before results can be used.' --icon=dialog-information
+
 else
 echo "There was an issue. Not installed"
+# Send notice that update has finished. 
+notify-send 'mr-updater' 'mutter-rounded had an issue and was not updated. Please use the "trigger-update.sh command from terminal to find out why".' --icon=dialog-information
 fi
